d986dec666262fec1b8b5ebcf70bb667c0513dea
Mickey Ristroph <mristroph@gmail.com>
2014-06-22 15:03:55 -0500

    basic drawing

---
+++ web-client/graffinity.js
@@ -0,0 +1,58 @@
+context = document.getElementById('currentCanvas').getContext("2d");
+
+$('#currentCanvas').mousedown(function(e){
+	var mouseX = e.pageX - this.offsetLeft;
+	var mouseY = e.pageY - this.offsetTop;
+	
+	paint = true;
+	addClick(e.pageX - this.offsetLeft, e.pageY - this.offsetTop);
+	redraw();
+    });
+
+$('#currentCanvas').mousemove(function(e){
+	if(paint){
+	    addClick(e.pageX - this.offsetLeft, e.pageY - this.offsetTop, true);
+	    redraw();
+	}
+    });
+
+
+$('#currentCanvas').mouseup(function(e){
+	paint = false;
+    });
+
+$('#currentCanvas').mouseleave(function(e){
+	paint = false;
+    });
+
+var clickX = new Array();
+var clickY = new Array();
+var clickDrag = new Array();
+var paint;
+
+function addClick(x, y, dragging)
+{
+    clickX.push(x);
+    clickY.push(y);
+    clickDrag.push(dragging);
+}
+
+function redraw(){
+    context.clearRect(0, 0, context.canvas.width, context.canvas.height); // Clears the canvas
+  
+    context.strokeStyle = "#444444";
+    context.lineJoin = "round";
+    context.lineWidth = 5;
+    
+    for(var i=0; i < clickX.length; i++) {
+	context.beginPath();
+	if(clickDrag[i] && i){
+	    context.moveTo(clickX[i-1], clickY[i-1]);
+	}else{
+	    context.moveTo(clickX[i]-1, clickY[i]);
+	}
+	context.lineTo(clickX[i], clickY[i]);
+	context.closePath();
+	context.stroke();
+    }
+}
