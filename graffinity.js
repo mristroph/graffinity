3bc0506ef54f21021ef526f872f3e37798ae6dcb
Kyle Varga <kyle.varga@mutualmobile.com>
2014-06-23 21:15:11 -0500

    add all the colors!

--- graffinity.js
+++ graffinity.js
@@ -81,6 +81,13 @@ $('#currentCanvas').mouseleave(function(e){
 	paint = false;
 });
 
+
+/* START: color palette */
+var CSS_COLOR_NAMES = ["AliceBlue","AntiqueWhite","Aqua","Aquamarine","Azure","Beige","Bisque","Black","BlanchedAlmond","Blue","BlueViolet","Brown","BurlyWood","CadetBlue","Chartreuse","Chocolate","Coral","CornflowerBlue","Cornsilk","Crimson","Cyan","DarkBlue","DarkCyan","DarkGoldenRod","DarkGray","DarkGrey","DarkGreen","DarkKhaki","DarkMagenta","DarkOliveGreen","Darkorange","DarkOrchid","DarkRed","DarkSalmon","DarkSeaGreen","DarkSlateBlue","DarkSlateGray","DarkSlateGrey","DarkTurquoise","DarkViolet","DeepPink","DeepSkyBlue","DimGray","DimGrey","DodgerBlue","FireBrick","FloralWhite","ForestGreen","Fuchsia","Gainsboro","GhostWhite","Gold","GoldenRod","Gray","Grey","Green","GreenYellow","HoneyDew","HotPink","IndianRed","Indigo","Ivory","Khaki","Lavender","LavenderBlush","LawnGreen","LemonChiffon","LightBlue","LightCoral","LightCyan","LightGoldenRodYellow","LightGray","LightGrey","LightGreen","LightPink","LightSalmon","LightSeaGreen","LightSkyBlue","LightSlateGray","LightSlateGrey","LightSteelBlue","LightYellow","Lime","LimeGreen","Linen","Magenta","Maroon","MediumAquaMarine","MediumBlue","MediumOrchid","MediumPurple","MediumSeaGreen","MediumSlateBlue","MediumSpringGreen","MediumTurquoise","MediumVioletRed","MidnightBlue","MintCream","MistyRose","Moccasin","NavajoWhite","Navy","OldLace","Olive","OliveDrab","Orange","OrangeRed","Orchid","PaleGoldenRod","PaleGreen","PaleTurquoise","PaleVioletRed","PapayaWhip","PeachPuff","Peru","Pink","Plum","PowderBlue","Purple","Red","RosyBrown","RoyalBlue","SaddleBrown","Salmon","SandyBrown","SeaGreen","SeaShell","Sienna","Silver","SkyBlue","SlateBlue","SlateGray","SlateGrey","Snow","SpringGreen","SteelBlue","Tan","Teal","Thistle","Tomato","Turquoise","Violet","Wheat","White","WhiteSmoke","Yellow","YellowGreen"];
+$.each(CSS_COLOR_NAMES, function( index, value ) {
+    $("#colors").append("<div id=\"" + value + "\"></div>" );
+});
+
 $('#colors div').each(function(index){
     $(this).css("background-color", this.id);
 });
@@ -88,16 +95,14 @@ $('#colors div').mousedown(function(e){
     color = this.id;
     makeCursor();
 });
+/* END: color palette */
 
-
+/* START: cursor code */
 loop();
-
-
 function loop() {
     makeCursor();
     setTimeout(loop, 1000);
 }
-
 function makeCursor() {
     var cursor = document.createElement('canvas');
     cursor.width = 10;
@@ -105,6 +110,6 @@ function makeCursor() {
     ctx = cursor.getContext('2d');
     ctx.fillStyle = color;
     ctx.fillRect(0,0,10,10);
-    
     document.body.style.cursor = 'url(' + cursor.toDataURL() + ') 5 5, auto';
 }
+/* END: cursor code */
